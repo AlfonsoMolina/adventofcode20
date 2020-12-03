7742832088365897ad2e40d94e6714f7339e9966
AlfonsoMolina <alfonso7193@gmail.com>
2020-12-03 11:31:50 +0100

    Day 3

---
+++ day03.py
@@ -0,0 +1,32 @@
+input_data = """..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#"""
+
+with open('adventofcode20/day03_input.txt', 'r') as f:    
+    input_data = f.read()
+
+input_data = [[ch for ch in line] for line in input_data.split('\n')]
+
+slope = [3, 1]
+
+def use_slope(forest, slope):
+    pos = [0,0]
+    tree_count = 0
+    while pos[0] < len(forest):
+        y, x = pos
+        tree_count += 1 if forest[y][x] == '#' else 0
+        y = y + slope[1]
+        x = (x + slope[0]) % len(forest[0]) 
+        pos = [y, x]
+    return tree_count
+
+tree_count = use_slope(input_data, slope)
+print(tree_count)
